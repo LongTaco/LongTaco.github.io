8926de714a9d4a5b90c3061ac377a8029f319cbf
trignomtry <106871964+trignomtry@users.noreply.github.com>
2024-09-24 13:44:04 -0400

    1

---
+++ main.js
@@ -0,0 +1,22 @@
+async function main(){
+    let greeting = document.getElementById("greeting");
+    let appKey = localStorage.getItem("appKey");
+    if(!appKey){
+      let newscript = document.createElement("script");
+      newscript.src = "https://mathwow.org/dash/wv/vortice.js";
+      document.head.appendChild(newscript);
+    } else {
+      let response = await fetch("https://mathwow.org/api/apps/" + appkey + "/userdata");
+      response = await response.json();
+      greeting.innerHTML = "Hello, " + response.name;
+    }
+    document.addEventListener("message", function(event){
+      if(event.data.error){
+        console.error("error logging in with vortice: " + event.data.error);
+      } else {
+        localStorage.setItem("appKey", event.data);
+        window.location.reload();
+      }
+    });
+  }
+  document.addEventListener("DOMContentLoaded", () => main);
